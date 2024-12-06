1fc198808377a7db2134c851527aa90c35d97d76
ItzTas <talessabini2@gmail.com>
2024-12-06 15:46:16 -0400

    build resolvida

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            input: './src/main.tsx',
-        },
-    },
 });
